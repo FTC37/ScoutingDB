47c7ac0c74aef8370f88c4675d0451f7de0effce
Taylor Blau <me@ttaylorr.com>
2013-10-06 15:57:06 -0400

    Adds basic preferences structure

---
+++ static/php/ref/pref.php
@@ -0,0 +1,20 @@
+<?php
+	$preferences = json_decode(
+		'{
+		   "team": {
+			  "team_name":"Team Name",
+			  "team_color":"#FFFFFF"
+		   }
+		   "database": {
+		      "user":"db_username",
+		      "pass":"db_pass",
+		      "addr":"localhost",
+		      "db_name":"robotics"
+		   }
+		}'
+	);
+
+	function getPreferencesAsJSON() {
+		return $preferences;
+	}
+?>
