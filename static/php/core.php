1f49e7476637567ea9c4c93a7c628424b36acd6a
unknown <tblau16@MJ12G5K.cranbrook.edu>
2013-10-11 12:25:48 -0400

    Changed core to an Application class

--- static/php/core.php
+++ static/php/core.php
@@ -1,22 +1,43 @@
 <?php
 	include "ref/pref.php";
 
-	function connect($path_to_config) {
-		$config = json_decode(getPreferencesAsJSON());
-
-		$username = $config['database']['user'];
-		$password = $config['database']['pass'];
-		$address = $config['database']['addr'];
-		$db_name = $config['database']['db_name'];
-		$charset = $config['database']['charset'];
+	class Application {
 	
-		try { 
-			$db = new PDO("mysql:host=" . $address . ";dbname=" . $db_name . ";charset=" . $charset, $username, $password);
-			return $db;
-		} catch(PDOException $e) {
-			return false;
+		$pdo = null;
+		$preferences = [];
+	
+		public function __construct() {
+			$preferences = json_decode(getPreferencesAsJSON());
+		
 		}
+	
+		public function __destruct() {
+			$this->pdo = null;
+		}
+	
+		public function connect() {
+			$username = $config['database']['user'];
+			$password = $config['database']['pass'];
+			$address = $config['database']['addr'];
+			$db_name = $config['database']['db_name'];
+			$charset = $config['database']['charset'];
+			
+			try { 
+				$pdo = new PDO("mysql:host=" . $address . ";dbname=" . $db_name . ";charset=" . $charset, $username, $password);
+				return $pdo;
+			} catch(PDOException $e) {
+				return false;
+			}		
+		}
+		
+		public function getDatabase() {
+			return $this->pdo;
+		}
+		
+		private function releaseDatabase() {
+			$this->pdo = null;
+		}
+	
 	}
-
 	
 ?>
